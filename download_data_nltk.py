6a092f0ae0dee865ff8f2d3bdd78319343541dcd
bert-IA <blouviot@gmail.com>
2024-09-18 21:02:48 +0200

    suppresion des fichier nltk.txt et nltk_download_data

--- download_data_nltk.py
+++
@@ -1,9 +0,0 @@
-import nltk
-
-# Lire le fichier nltk.txt et télécharger les ressources spécifiées
-with open('nltk.txt') as f:
-    resources = f.read().splitlines()
-
-# Télécharger les ressources dans le répertoire spécifié
-for resource in resources:
-    nltk.download(resource, download_dir='/usr/local/share/nltk_data')
