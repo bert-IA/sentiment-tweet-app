f69c67fe21a45e5aa2b5f062bb33b5bc562ef76c
bert-IA <blouviot@gmail.com>
2024-09-23 11:38:25 +0200

    modification test_app.py version et sentiment pour la sortie

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,14 +7,14 @@ client = TestClient(app)
 def test_health_check():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"health_check": "ok", "model_version": "1.0.0"}
+    assert response.json() == {"health_check": "ok", "model_version": "0.1.0"}
 
 def test_predict_positive():
     response = client.post("/predict", json={"text": "I love this service!"})
     assert response.status_code == 200
-    assert response.json() == {"prediction": "tweet_positif"}
+    assert response.json() == {"sentiment": "tweet_positif"}
 
 def test_predict_negative():
     response = client.post("/predict", json={"text": "I hate this service!"})
     assert response.status_code == 200
-    assert response.json() == {"prediction": "tweet_négatif"}
+    assert response.json() == {"sentiment": "tweet_négatif"}
